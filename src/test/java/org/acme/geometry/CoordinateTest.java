09d6bc603e82e871724e4c1d7c6c40f245992ba9
Damien D'ARRAS <damien.d-arras@ensg.eu>
2023-01-11 15:15:25 +0100

    Implementation of Geometry and tests on Coordinates

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -9,10 +9,16 @@ public class CoordinateTest {
 
 	@Test
 	public void testDefaultConstructor(){
-		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(0.0, c.getX(), EPSILON);
+		Assert.assertEquals(0.0, c.getY(), EPSILON);
+	}
+
+	@Test
+	public void testParamsConstructor(){
+		Coordinate c = new Coordinate(1.0,2.0);
+		Assert.assertEquals(1.0, c.getX(), EPSILON);
+		Assert.assertEquals(2.0, c.getY(), EPSILON);
 	}
 
 }
